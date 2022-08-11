bb514dd9dab1bee288040d6f535afdb26f40974a
mmdmthr <muhhari@gmail.com>
2022-08-11 21:33:11 +0700

    explain go syntax

--- hello.go
+++ hello.go
@@ -1,11 +1,19 @@
 package main
 
+// Di Go, setiap program adalah bagian dari sebuah package.
+// Kita mendefinisikan ini menggunakan kata kunci package.
+// Dalam contoh ini, program termasuk dalam paket main.
+
 import (
 	"fmt"
-	"os"
 )
 
+// Import ("fmt") memungkinkan kita mengimpor file yang disertakan dalam paket fmt.
+// Go mengabaikan white space seperti enter.
+
 func main() {
-	fmt.Printf("hello, world\n")
-	fmt.Println(os.Getenv("USER"), ", Let's be friends!")
+	// func main() {} adalah sebuah fungsi. Kode di dalam tanda kurung kurawalnya {} akan dieksekusi.
+	fmt.Printf("Hello, World!")
+	// fmt.Println() adalah fungsi yang tersedia dari paket fmt.
+	// Digunakan untuk menampilkan/mencetak teks. Dalam contoh kita ini akan menampilkan "Hello World!".
 }
