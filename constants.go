2bd0dee64160ed459e81c6311cb7af066f2f44aa
mmdmthr <muhhari@gmail.com>
2022-08-13 05:33:02 +0700

    constant in go

---
+++ constants.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+/* Jika sebuah variable memiliki nilai yang tetap dan tidak dapat diubah
+kita dapat menggunakan kata kunci const */
+
+const PI = 3.14
+
+// nilai harus diisi dalam deklarasi constant
+
+// Nama constant mengikuti aturan penamaan yang sama dengan variabel
+// Nama constant biasanya ditulis dengan huruf besar (untuk memudahkan identifikasi dan diferensiasi dari variabel)
+// Constant dapat dideklarasikan baik di dalam maupun di luar suatu fungsi */
+
+func main() {
+	fmt.Printf("hello, world\n")
+}
