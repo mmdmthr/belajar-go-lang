62b9c9bb2f9a2f022bd3d0652574976fa3606e70
mmdmthr <muhhari@gmail.com>
2022-12-11 14:40:01 +0700

    add switch topic

---
+++ switch.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// switch digunakan untuk memilih 
+	// satu dari beberapa baris kode untuk dieksekusi
+	day := 8
+
+	switch day {
+	case 1:
+	  fmt.Println("Monday")
+	case 2:
+	  fmt.Println("Tuesday")
+	case 3:
+	  fmt.Println("Wednesday")
+	case 4:
+	  fmt.Println("Thursday")
+	case 5:
+	  fmt.Println("Friday")
+	case 6:
+	  fmt.Println("Saturday")
+	case 7:
+	  fmt.Println("Sunday")
+	default:
+      fmt.Println("Not a weekday")
+	}
+
+	// switch juga dapat digunakan untuk mengevaluasi 
+	// beberapa value sekaligus dengan koma pada case (case 1,2)
+}
